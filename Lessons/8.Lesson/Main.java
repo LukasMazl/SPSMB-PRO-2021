a38557d427beae8e30a7a78300da0bfc70cf5253
Vojtěch Vít <90755402+vojtechos@users.noreply.github.com>
2021-11-10 10:18:47 +0100

    Add files via upload

---
+++ Lessons/8.Lesson/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*
+	    LocalDateTime currentDate = LocalDateTime.now();
+        System.out.println(currentDate.format(DateTimeFormatter.ofPattern("yyyy//MM-dd")));
+         */
+    }
+}
