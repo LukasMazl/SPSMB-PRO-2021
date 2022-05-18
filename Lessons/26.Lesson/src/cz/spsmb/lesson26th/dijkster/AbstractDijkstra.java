bd2301ab1b24b4d162358f70bbf35f09b1d894ad
mazl <mazl@spsmb.cz>
2022-05-18 13:44:18 +0200

    Added Dijkstra alg

---
+++ Lessons/26.Lesson/src/cz/spsmb/lesson26th/dijkster/AbstractDijkstra.java
@@ -0,0 +1,57 @@
+package cz.spsmb.lesson26th.dijkster;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public abstract class AbstractDijkstra {
+
+
+    public void run(Node startNode) {
+        LinkedList<Node> currentNodes = new LinkedList<>();
+        currentNodes.push(startNode);
+        while (!currentNodes.isEmpty()) {
+            Node currentNode = currentNodes.pop();
+            List<Node> neighbours = findAllNeighbours(currentNode);
+            for (Node node : neighbours) {
+                Edge edge = findEdge(node, currentNode);
+                int totalSize = currentNode.getValue() + edge.getValue();
+                if (node.getValue() > totalSize) {
+                    node.setValue(totalSize);
+                }
+            }
+            currentNode.markUsed();
+        }
+    }
+
+    protected abstract Edge findEdge(Node node, Node currentNode);
+
+    protected abstract List<Node> findAllNeighbours(Node startNode);
+
+    private class Edge {
+        private int value;
+
+        public int getValue() {
+            return value;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+    }
+
+    private class Node {
+
+        private int value;
+
+        public int getValue() {
+            return 0;
+        }
+
+        public void setValue(int totalSize) {
+        }
+
+        public void markUsed() {
+
+        }
+    }
+}
